f14926a84615c0172d85888d1bad56471f18f276
Marc Hinte <m.hinte@neusta.de>
2024-08-28 20:25:19 +0200

    Remove not needed pal-dvd-preset.json

--- constants.py
+++ constants.py
@@ -1,4 +1,3 @@
 INPUT_FOLDER = "videos\\input\\"
 OUTPUT_FOLDER = "videos\\output\\"
 HANDBRAKE_CLI_PATH = "handbrake/HandBrakeCLI.exe"
-HANDBRAKE_DVD_PRESET_PATH = "handbrake/presets/pal-dvd-preset.json"
