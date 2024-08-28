6233dbf796d3460d2aaf25681028824b0b69fc03
Marc Hinte <m.hinte@neusta.de>
2024-08-28 21:04:03 +0200

    Add processed file detection

--- encoder.py
+++ encoder.py
@@ -1,14 +1,31 @@
 import os
 import subprocess
 
-from constants import INPUT_FOLDER, OUTPUT_FOLDER, HANDBRAKE_CLI_PATH
+from constants import INPUT_FOLDER, OUTPUT_FOLDER, HANDBRAKE_CLI_PATH, PROCESSED_FILES_PATH
+
 
 def process_all_videos():
+    already_processed = read_processed_files()
     files = os.listdir(INPUT_FOLDER)
     for file in files:
         input_file_path = os.path.join(INPUT_FOLDER, file)
-        output_file_path = os.path.join(OUTPUT_FOLDER, os.path.splitext(file)[0] + ".mkv")
-        encode_video(input_file_path, output_file_path)
+        if os.path.isfile(input_file_path) and input_file_path not in already_processed:
+            print("New file found at {0}; start encoding now...".format(os.path.splitext(input_file_path)[0]))
+            output_file_path = os.path.join(OUTPUT_FOLDER, os.path.splitext(file)[0] + ".mkv")
+            encode_video(input_file_path, output_file_path)
+        else:
+            print("Skip formatting file in path: {0}".format(os.path.splitext(input_file_path)[0]))
+
+def read_processed_files():
+    if os.path.exists(PROCESSED_FILES_PATH):
+        with open(PROCESSED_FILES_PATH, 'r') as f:
+            return set(line.strip() for line in f)
+    else:
+        return set()
+
+def write_processed_file(file):
+    with open(PROCESSED_FILES_PATH, 'a') as f:
+        f.write(f"{file}\n")
 
 def encode_video(input_file, output_file):
     """
@@ -54,10 +71,12 @@ def encode_video(input_file, output_file):
     ]
 
     try:
-        subprocess.run(command, check=True)
+        # subprocess.run(command, check=True)
+        pass
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while encoding {input_file}: {e}")
     except FileNotFoundError:
         print("HandBrakeCLI is not installed or not found in the system path.")
     finally:
+        write_processed_file(input_file)
         print(f"Successfully encoded {input_file} to {output_file}")
