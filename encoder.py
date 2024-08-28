244403aa79733960f4a84594f9efccb3869bcbbb
Marc Hinte <m.hinte@neusta.de>
2024-08-28 20:24:56 +0200

    Update handbrake settings

--- encoder.py
+++ encoder.py
@@ -1,13 +1,13 @@
 import os
 import subprocess
 
-from constants import INPUT_FOLDER, OUTPUT_FOLDER, HANDBRAKE_CLI_PATH, HANDBRAKE_DVD_PRESET_PATH
+from constants import INPUT_FOLDER, OUTPUT_FOLDER, HANDBRAKE_CLI_PATH
 
 def process_all_videos():
     files = os.listdir(INPUT_FOLDER)
     for file in files:
         input_file_path = os.path.join(INPUT_FOLDER, file)
-        output_file_path = os.path.join(OUTPUT_FOLDER, os.path.splitext(file)[0] + "_encoded2.mkv")
+        output_file_path = os.path.join(OUTPUT_FOLDER, os.path.splitext(file)[0] + ".mkv")
         encode_video(input_file_path, output_file_path)
 
 def encode_video(input_file, output_file):
@@ -18,6 +18,11 @@ def encode_video(input_file, output_file):
         input_file (str): Path to the input video file.
         output_file (str): Path to save the encoded output video file.
     """
+
+    # Ensure the output directory exists
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+    # HandBrake settings for a pal dvd
     command = [
         HANDBRAKE_CLI_PATH,
         '--input', input_file,
@@ -29,16 +34,18 @@ def encode_video(input_file, output_file):
         '--vfr',
         '--unsharp-tune', 'fine',
         '--hqdn3d', 'light',
-        '--crop', 'auto',
         '--width', '720',
         '--height', '576',
-        '--audio-lang-list', 'deu, eng',
+        '--auto-anamorphic',
+        '--audio-lang-list', 'deu,eng',
+        '--aname', 'Deutsch,English',
         '--first-audio',
         '--aencoder', 'av_aac',
         '--audio-copy-mask','aac,ac3',
         '--audio-fallback', 'av_aac',
         '--mixdown', 'dpl1',
-        '--subtitle-lang-list', 'deu, eng',
+        '--subtitle-lang-list', 'deu,eng',
+        '--subname', 'Deutsch,English',
         '--first-subtitle',
         '--aq', '5',
         '--multi-pass',
@@ -48,14 +55,9 @@ def encode_video(input_file, output_file):
 
     try:
         subprocess.run(command, check=True)
-        print(f"Successfully encoded {input_file} to {output_file}")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while encoding {input_file}: {e}")
     except FileNotFoundError:
         print("HandBrakeCLI is not installed or not found in the system path.")
-
-# def encode_video(input_path, output_path):
-#     print(f"Kodiere: {input_path}")
-#     command = f'"{HANDBRAKE_CLI_PATH}" -i "{input_path}" -o "{output_path}" --preset-import-file {HANDBRAKE_DVD_PRESET_PATH}'
-#     subprocess.run(command, shell=True)
-#     print(f"Kodierung abgeschlossen: {output_path}")
+    finally:
+        print(f"Successfully encoded {input_file} to {output_file}")
