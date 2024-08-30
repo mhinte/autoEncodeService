1eff15386a19c5efbf86ab1153f6872d7b525933
Marc Hinte <m.hinte@neusta.de>
2024-08-30 12:04:20 +0200

    move processed_files.txt to temp folder

--- src/helper/constants.py
+++ src/helper/constants.py
@@ -2,5 +2,5 @@
 
 INPUT_FOLDER = "videos\\input\\"
 OUTPUT_FOLDER = "videos\\output\\"
-PROCESSED_FILES_PATH = "processed_files.txt"
+PROCESSED_FILES_PATH = "temp/processed_files.txt"
 HANDBRAKE_CLI_PATH = "src/HandBrakeCLI.exe"
