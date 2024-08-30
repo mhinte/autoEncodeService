402b3f78c76d8992dfedc142444e697ec3064506
Marc Hinte <m.hinte@neusta.de>
2024-08-30 08:26:13 +0200

    Move logger initialization to top of file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import logging
 
 from encoder import process_all_videos
 
+FORMAT = '%(asctime)s - %(levelname)s : %(message)s'
+logging.basicConfig(filename='logs/encoder.log', level=logging.INFO, format=FORMAT)
 logger = logging.getLogger(__name__)
 MONITORING = False
 
@@ -13,8 +15,6 @@ def monitor_folder():
 
 
 if __name__ == '__main__':
-    FORMAT = '%(asctime)s - %(levelname)s : %(message)s'
-    logging.basicConfig(filename='logs/encoder.log', level=logging.INFO, format=FORMAT)
     try:
         if MONITORING:
             monitor_folder()
